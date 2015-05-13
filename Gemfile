037c79a4a32069033037d3f65a4cd8d967c29b23
Rodney Cullen <rodcul@gmail.com>
2015-05-13 11:22:33 +0100

    Version 1: New game page

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'battleships' # if you are using Ben's gem
+
+group :development, :test do
+  gem 'capybara'
+  gem 'cucumber'
+  gem 'cucumber-sinatra'
+  gem 'launchy'
+  gem 'rspec'
+  gem 'shotgun'
+end
---
+++ lib/battleships_web.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+require 'battleships'
+
+class BattleshipsWeb < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "..", "views") }
+
+  get '/' do
+    erb :index
+  end
+
+  get '/game/new' do
+    erb :game_new
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
